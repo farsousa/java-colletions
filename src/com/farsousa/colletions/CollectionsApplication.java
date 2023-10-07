ca8ac4aa13747f2806a13c1224a54213fe2a9785
farsousa <andersoncaj26@gmail.com>
2023-10-07 18:03:12 -0300

    commit inicial

---
+++ src/com/farsousa/colletions/CollectionsApplication.java
@@ -0,0 +1,10 @@
+package com.farsousa.colletions;
+
+public class CollectionsApplication {
+	
+	public static void main(String [] args) {
+		System.out.println("Olá Mundo!");
+		System.out.println("Olá Mundo!");
+	}
+
+}
